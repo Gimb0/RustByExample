b8dc04c0d44a7d2cb07f7d16c4309f36e390bb16
Gimb0 <18228506+Gimb0@users.noreply.github.com>
2022-04-23 21:44:24 +1000

    Play around with printing

---
+++ src/PrintFormatting.rs
@@ -0,0 +1,24 @@
+// This program plays around with different formats of printing
+
+fn main() {
+    // No vars
+    println!("Hello!\n");
+
+    // int automatically converted to string
+    println!("{}\n", 1337);
+
+    // reuse vars
+    println!("{0} SYN {1}\n{1} SYN/ACK {0}\n{0} ACK {1}\n", "Alice", "Bob");
+
+    // named vars
+    println!("My name is {name}.\n", name="Gimbo");
+
+    // specify var format (example is turn int to binary)
+    println!("{} of {:b} people understand binary\n", 1, 2);
+
+    // print text pushed to right without using spaces or \t
+    println!("{var:>width$}\n", var="Hi", width=6);
+
+    // print number with padding \t
+    println!("0x{var:0>width$}", var=4141, width=8);
+}
