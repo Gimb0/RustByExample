39712eefe503652e26f2acbf65b2791310e80d95
Gimb0 <18228506+Gimb0@users.noreply.github.com>
2022-04-24 21:16:22 +1000

    Playing around with primitives

---
+++ src/Primitives/prims.rs
@@ -0,0 +1,14 @@
+// This program covers the primitive data types
+fn main() {
+    let _logical: bool = true;
+
+    let _a_float: f64 = 1.0; //64 bit float
+    let _an_int: i32 = 5; // 32 bit int
+
+    // mutable variables value can be changed
+    let mut inferred_type = 12;
+    inferred_type = 4294967296i64;
+
+    // change var type using let
+    let inferred_type = true;
+}
